dc73f0c0b77114d3acc1c017ef903b7a49e74e28
= <=>
2019-12-19 16:16:10 +0200

    toolbar&translation 1

--- employee_management_project/employee_management_project/urls.py
+++ employee_management_project/employee_management_project/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [     
+import debug_toolbar
+
+urlpatterns = [
     path('', include('management_app.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('management_app.urls')),
+    path('admin/', admin.site.urls), 
+)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
