f44af245d9385956dca8eb18a7ad014c9cda27ee
= <=>
2019-12-26 13:58:05 +0200

    added id to serializers

--- employee_management_project/management_app/api/serializers.py
+++ employee_management_project/management_app/api/serializers.py
@@ -8,7 +8,7 @@ class ManagerSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Manager
-        fields = ('url', 'company_name', 'company', 'name', 'email')
+        fields = ('url', 'id', 'company_name', 'company', 'name', 'email')
 
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
@@ -16,47 +16,47 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Company
-        fields = ('url', 'name', 'weekly_hours_limit', 'managers')
+        fields = ('url', 'id', 'name', 'weekly_hours_limit', 'managers')
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
-        fields = ('url', 'name',)
+        fields = ('url', 'id', 'name',)
 
 
 class JobSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Job
-        fields = ('url', 'company', 'name',)
+        fields = ('url', 'id', 'company', 'name',)
 
 
 class WorkPlaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WorkPlace
-        fields = ('url', 'job', 'employee', 'status',)
+        fields = ('url', 'id', 'job', 'employee', 'status',)
 
 
 class WorkPlaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WorkPlace
-        fields = ('url', 'job', 'employee', 'status',)
+        fields = ('url', 'id', 'job', 'employee', 'status',)
 
 
 class WorkTimeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WorkTime
-        fields = ('url', 'workplace', 'date', 'hours_worked',)
+        fields = ('url', 'id', 'workplace', 'date', 'hours_worked',)
 
 
 class StatisticsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Statistics
-        fields = ('url', 'workplace', 'hours_total', 'timestamp',)
+        fields = ('url', 'id', 'workplace', 'hours_total', 'timestamp',)
 
 
 class StatisticsUpdateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Statistics
-        fields = ('url', 'workplace', 'hours_total', 'timestamp',)
+        fields = ('url', 'id', 'workplace', 'hours_total', 'timestamp',)
         read_only_fields = ('workplace',)
